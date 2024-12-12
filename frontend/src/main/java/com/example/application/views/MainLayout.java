77de58b278413e8f96e1f151972eab29b8a843c1
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:31:22 -0800

    Update MainLayout.java

--- frontend/src/main/java/com/example/application/views/MainLayout.java
+++ frontend/src/main/java/com/example/application/views/MainLayout.java
@@ -1,33 +1,29 @@
 package com.example.application.views;
 
-
-import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.html.Footer;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.orderedlayout.Scroller;
-import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
-import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.vaadin.lineawesome.LineAwesomeIcon;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.router.PageTitle;
 
-/**
- * The main view is a top-level placeholder for other views.
- */
 public class MainLayout extends AppLayout {
-
     private H2 viewTitle;
-
-    public MainLayout() {
+    public MainLayout() 
+    {
         setPrimarySection(Section.DRAWER);
         addDrawerContent();
         addHeaderContent();
     }
 
-    private void addHeaderContent() {
+    private void addHeaderContent() 
+    {
         DrawerToggle toggle = new DrawerToggle();
         toggle.setAriaLabel("Menu toggle");
 
@@ -37,33 +33,31 @@ public class MainLayout extends AppLayout {
         addToNavbar(true, toggle, viewTitle);
     }
 
-    private void addDrawerContent() {
+    private void addDrawerContent() 
+    {
         H1 appName = new H1("frontend");
         appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
         Header header = new Header(appName);
 
         Scroller scroller = new Scroller(createNavigation());
-
         addToDrawer(header, scroller, createFooter());
     }
 
-    private SideNav createNavigation() {
+    private SideNav createNavigation() 
+    {
         SideNav nav = new SideNav();
-
-//        nav.addItem(new SideNavItem("Hello World", HelloWorldView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
-//        nav.addItem(new SideNavItem("About", AboutView.class, LineAwesomeIcon.FILE.create()));
-
         return nav;
     }
 
-    private Footer createFooter() {
+    private Footer createFooter() 
+    {
         Footer layout = new Footer();
-
         return layout;
     }
 
     @Override
-    protected void afterNavigation() {
+    protected void afterNavigation() 
+    {
         super.afterNavigation();
         viewTitle.setText(getCurrentPageTitle());
     }
